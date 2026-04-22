c7fecffe78218bdda75408b8e60125b9ee0b8481
Rushish Mewada <rushish.mewada@asmitagroup.com>
2026-04-22 12:00:40 +0530

    Upload CSS for Edit Pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  allowedDevOrigins: ['157.230.247.182', '172.16.40.172'],
+  allowedDevOrigins: ['157.230.247.182', '172.16.40.172', '192.168.198.1'],
 };
 
 export default nextConfig;
